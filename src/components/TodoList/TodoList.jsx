dd1dc066c781746704a25fa3801ee657071f5cff
king0-king0 <oyadkarem@gmail.com>
2025-02-21 15:06:09 +0100

    S10. L03. Structuring and Styling To-Do List

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,23 +1,36 @@
+import styles from "./TodoList.module.css";
+
 export function TodoList({ todos }) {
   return (
     <section>
       <h3>To-Do's</h3>
 
-      <ul>
+      <ul className={styles.TodoList}>
         {todos.map((todo) => (
-          <li key={todo.id}>
-            <input
-              type="checkbox"
-              name="completed"
-              defaultChecked={todo.completed}
-            />
+          <li
+            key={todo.id}
+            className={styles.TodoListItem}
+            data-completed={todo.completed}
+          >
+            <div className={styles.Content}>
+              <input
+                type="checkbox"
+                name="completed"
+                defaultChecked={todo.completed}
+                className={styles.Status}
+              />
 
-            <div>
-              {todo.name}
-              <br />
-              {todo.description}
-              <br />
-              {todo.deadline} {todo.priority !== "none" && todo.priority}
+              <div className={styles.Info}>
+                {todo.name}
+
+                {todo.description && (
+                  <span className={styles.Description}>{todo.description}</span>
+                )}
+
+                <div className={styles.AdditionalInfo}>
+                  {todo.deadline} {todo.priority !== "none" && todo.priority}
+                </div>
+              </div>
             </div>
           </li>
         ))}

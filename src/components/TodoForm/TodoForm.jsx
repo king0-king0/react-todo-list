235f9c6c102d4830ae7ce416cfa7e5aef34b165e
king0-king0 <oyadkarem@gmail.com>
2025-02-23 19:50:09 +0100

    S12. L02. Using React Hook Form Library

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,26 +1,23 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { PRIORITY_DEFAULT } from "../../constants/priorities";
 import { TodoFormFields } from "../TodoFormFields/TodoFormFields";
 import styles from "./TodoForm.module.css";
 
 export function TodoForm({ onCreate }) {
   const [showAllFields, setShowAllFields] = useState(false);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    const { elements } = event.target;
-    if (elements.name.value === "") return;
-
-    onCreate({
-      name: elements.name.value,
-      description: elements.description?.value ?? "",
-      deadline: elements.deadline?.value ?? "",
-      priority: elements.priority?.value ?? PRIORITY_DEFAULT,
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      description: "",
+      deadline: "",
+      priority: PRIORITY_DEFAULT,
       completed: false,
-    });
+    },
+  });
 
-    event.target.reset();
+  function handleCreate(data) {
+    onCreate(data);
+    reset();
   }
 
   return (
@@ -32,8 +29,8 @@ export function TodoForm({ onCreate }) {
         </button>
       </h3>
 
-      <form className={styles.Form} onSubmit={handleSubmit}>
-        <TodoFormFields showAllFields={showAllFields} />
+      <form className={styles.Form} onSubmit={handleSubmit(handleCreate)}>
+        <TodoFormFields showAllFields={showAllFields} register={register} />
         <input type="submit" value="Add" />
       </form>
     </section>
--- src/components/TodoFormFields/TodoFormFields.jsx
+++ src/components/TodoFormFields/TodoFormFields.jsx
@@ -1,7 +1,7 @@
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
 import styles from "./TodoFormFields.module.css";
 
-export function TodoFormFields({ todo = {}, showAllFields = true }) {
+export function TodoFormFields({ todo = {}, showAllFields = true, register }) {
   return (
     <div className={styles.FormFields}>
       <div className={styles.FormField}>
@@ -9,12 +9,9 @@ export function TodoFormFields({ todo = {}, showAllFields = true }) {
           type="text"
           aria-label="Name*"
           placeholder="Name*"
-          name="name"
           autoComplete="off"
           defaultValue={todo.name}
-          required
-          minLength={3}
-          maxLength={50}
+          {...register("name", { required: true, minLength: 3, maxLength: 50 })}
         />
       </div>
 
@@ -24,10 +21,9 @@ export function TodoFormFields({ todo = {}, showAllFields = true }) {
             <textarea
               aria-label="Description"
               placeholder="Description"
-              name="description"
               rows="3"
               defaultValue={todo.description}
-              maxLength={200}
+              {...register("description", { maxLength: 200 })}
             />
           </div>
 
@@ -37,9 +33,13 @@ export function TodoFormFields({ todo = {}, showAllFields = true }) {
               <input
                 type="date"
                 id="deadline"
-                name="deadline"
                 defaultValue={todo.deadline}
-                min={new Date().toISOString().split("T")[0]}
+                {...register(
+                  "deadline",
+                  !todo.id && {
+                    min: new Date().toISOString().split("T")[0],
+                  }
+                )}
               />
             </div>
 
@@ -48,7 +48,7 @@ export function TodoFormFields({ todo = {}, showAllFields = true }) {
               <select
                 defaultValue={todo.priority ?? PRIORITY_DEFAULT}
                 id="priority"
-                name="priority"
+                {...register("priority")}
               >
                 {Object.entries(PRIORITIES).map(([key, { label }]) => (
                   <option key={key} value={key}>

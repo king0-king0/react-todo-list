97312c233ce9dfe1522e397edd2ee1f569841cfe
king0-king0 <oyadkarem@gmail.com>
2025-02-24 21:53:44 +0100

    S12. L06. Provide Custom Validation for Deadline with Yup

--- src/schemas/todo.js
+++ src/schemas/todo.js
@@ -1,7 +1,15 @@
 import * as Yup from "yup";
 import { PRIORITIES } from "../constants/priorities";
 
-export function getTodoSchema() {
+export function getTodoSchema({ isNew = false } = {}) {
+  const deadlineRule = Yup.string()
+    .nullable()
+    .transform((value, originalValue) => (originalValue === "" ? null : value))
+    .matches(
+      /^\d{4}-\d{2}-\d{2}$/,
+      "Deadline should be valid date in YYYY-MM-DD format"
+    );
+
   return Yup.object().shape({
     name: Yup.string()
       .required("Name is required")
@@ -11,7 +19,16 @@ export function getTodoSchema() {
       200,
       "Description should have max length of 200 characters"
     ),
-    deadline: Yup.string(),
+    deadline: isNew
+      ? deadlineRule.test(
+          "is-future-date",
+          "Deadline can't be date in the past",
+          (value) => {
+            const today = new Date().toISOString().split("T")[0];
+            return value ? value >= today : true;
+          }
+        )
+      : deadlineRule,
     priority: Yup.string()
       .required("Priority is not valid value")
       .oneOf(Object.keys(PRIORITIES), "Priority is not valid value"),
